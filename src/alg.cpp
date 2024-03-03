a5c41347e7fb679e089d679c7baf78bc507a7017
TolyaChepaykin <160387810+TolyaChepaykin@users.noreply.github.com>
2024-03-03 13:42:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
-// Copyright 2021 NNTU-CS
 #include <iostream>
 #include "alg.h"
 int gcd(int a, int b) {
-    std::cout << gcd(10,15) << std::endl;
+    std::cout << gcd(a, b) << std::endl;
     return 0;
 }
